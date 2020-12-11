70bbf7abb2222cdb28b5779e3b17c66bdd043588
sunil-pawar <saccha.sunil@gmail.com>
2020-12-11 16:07:52 +0000

    roles created

--- terraform_vpc/vpc.tf
+++ terraform_vpc/vpc.tf
@@ -6,23 +6,23 @@ data "aws_availability_zones" "available" {}
 
 # Define a vpc
 resource "aws_vpc" "test_vpc" {
-  cidr_block = "${var.vpc_cidr}"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "var.test_vpc"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+    Name = var.test_vpc
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 
 # Internet gateway for the public subnet
 resource "aws_internet_gateway" "test_ig" {
-  vpc_id = "${aws_vpc.test_vpc.id}"
+  vpc_id = aws_vpc.test_vpc.id
   tags = {
-    Name = "test_ig"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+     Name = var.test_vpc
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 
@@ -34,81 +34,81 @@ resource "aws_eip" "nat_eip" {
 }
 # NAT 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat_eip.id}"
-  subnet_id     = "${aws_subnet.test_private_sn_01.id}"
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.test_private_sn_01.id
   depends_on    = [aws_internet_gateway.test_ig]
   tags = {
     Name        = "nat"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 # Public subnet 1
 resource "aws_subnet" "test_public_sn_01" {
-  vpc_id = "${aws_vpc.test_vpc.id}"
-  cidr_block = "${var.test_public_01_cidr}"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = var.test_public_01_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
    map_public_ip_on_launch = true
   tags = {
     Name = "test_public_sn_01"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 
 #  Private subnet 
 resource "aws_subnet" "test_private_sn_01" {
-  vpc_id                  = "${aws_vpc.test_vpc.id}"
-  cidr_block              = "${var.test_private_02_cidr}"
-availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  vpc_id                  = aws_vpc.test_vpc.id
+  cidr_block              = var.test_private_02_cidr
+availability_zone = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = false
   tags = {
     Name = "test_private_sn_01"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 
 # Routing table for private subnet */
 resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.test_vpc.id}"
+  vpc_id = aws_vpc.test_vpc.id
   tags = {
     Name        = "private-route-table"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 resource "aws_route" "private_nat_gateway" {
-  route_table_id         = "${aws_route_table.private.id}"
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 #associate rout tabel to private subnet
 resource "aws_route_table_association" "private" {
-  subnet_id      = "${aws_subnet.test_private_sn_01.id}"
-  route_table_id = "${aws_route_table.private.id}"
+  subnet_id      = aws_subnet.test_private_sn_01.id
+  route_table_id = aws_route_table.private.id
 }
 
 
 
 # Routing table for public subnet 1
 resource "aws_route_table" "test_public_sn_rt_01" {
-  vpc_id = "${aws_vpc.test_vpc.id}"
+  vpc_id = aws_vpc.test_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.test_ig.id}"
+    gateway_id = aws_internet_gateway.test_ig.id
   }
   tags = {
     Name = "test_public_sn_rt_01"
-    stage = "var.stage"
-    billing_tag = "var.billing_tag"
+    stage = var.stage
+    billing_tag = var.billing_tag
   }
 }
 
 # Associate the routing table to public subnet 1
 resource "aws_route_table_association" "test_public_sn_rt_01_assn" {
-  subnet_id = "${aws_subnet.test_public_sn_01.id}"
-  route_table_id = "${aws_route_table.test_public_sn_rt_01.id}"
+  subnet_id = aws_subnet.test_public_sn_01.id
+  route_table_id = aws_route_table.test_public_sn_rt_01.id
 }
 
 
@@ -116,7 +116,7 @@ resource "aws_route_table_association" "test_public_sn_rt_01_assn" {
 resource "aws_security_group" "test_public_sg" {
     name = "test_public_sg"
     description = "Test public access security group"
-    vpc_id = "${aws_vpc.test_vpc.id}"
+    vpc_id = aws_vpc.test_vpc.id
     ingress {
         from_port = 22
         to_port = 22
@@ -145,7 +145,7 @@ resource "aws_security_group" "test_public_sg" {
         from_port = 0
         to_port = 0
         protocol = "tcp"
-        cidr_blocks = [ "${var.test_public_01_cidr}" , "${var.test_private_02_cidr}" ]
+        cidr_blocks = [ var.test_public_01_cidr , var.test_private_02_cidr ]
     }
     egress {
     # allow all traffic to private SN
@@ -157,7 +157,7 @@ resource "aws_security_group" "test_public_sg" {
 
      tags = {
         Name = "test_public_sg"
-        stage = "${var.stage}"
-        billing_tag = "${var.billing_tag}"
+        stage = var.stage
+        billing_tag = var.billing_tag
     }
 }
